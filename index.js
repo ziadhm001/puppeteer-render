5ba4d0e976976bed4cc8cd1a0c4825965626e246
Ziad Hazem <71240243+ziadhm001@users.noreply.github.com>
2023-08-15 19:58:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { bot } = require("./bot");
+const { bot } = require("./bot.js");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+  bot(10000,10000);
 });
 
 app.get("/", (req, res) => {
